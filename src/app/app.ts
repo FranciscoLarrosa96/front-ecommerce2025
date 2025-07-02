9c6c2e0c0e9ae9a4e42bfd25feb2f054bed02c37
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-07-02 17:39:15 -0300

    Feat: Products and filters added

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,14 +4,43 @@ import { RouterOutlet } from '@angular/router';
 import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements AfterViewInit {
   protected title = 'front';
   isDrawerOpen = false;
-
+  productos = [
+    {
+      nombre: 'MSI RTX 4060 Ti Ventus 8GB',
+      precio: 772000,
+      precioOriginal: 837900,
+      thumb: 'assets/img/rtx4060ti-thumb.avif',
+      imagen: 'assets/img/rtx4060ti.avif',
+    },
+    {
+      nombre: 'Gigabyte RTX 4070 12GB',
+      precio: 1015000,
+      precioOriginal: null,
+      thumb: 'assets/img/rtx4070-thumb.avif',
+      imagen: 'assets/img/rtx4070.avif',
+    },
+    {
+      nombre: 'ASUS ROG Strix RTX 4080 16GB',
+      precio: 1500000,
+      precioOriginal: 1600000,
+      thumb: 'assets/img/rtx4080-thumb.avif',
+      imagen: 'assets/img/rtx4080.avif',
+    },
+    {
+      nombre: 'Gigabyte RTX 4090 24GB',
+      precio: 2500000,
+      precioOriginal: 2700000,
+      thumb: 'assets/img/rtx4090-thumb.avif',
+      imagen: 'assets/img/rtx4090.avif',
+    }
+  ];
   ngAfterViewInit() {
     // Le damos un pequeño delay para evitar conflictos
     setTimeout(() => {
