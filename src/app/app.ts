3ce3e61056f72bd989eec31d516b85011ec18b8f
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-07-02 10:32:12 -0300

    Feat: Drawer and header added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
   protected title = 'front';
+  isDrawerOpen = false;
+
+  ngAfterViewInit() {
+    // Le damos un pequeño delay para evitar conflictos
+    setTimeout(() => {
+      AOS.init({
+        duration: 800,
+        once: false,
+        easing: 'ease-in-out',
+      });
+    }, 0);
+  }
 }
