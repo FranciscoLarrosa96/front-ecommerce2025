b54ed78234b549ce820358b46009c13ecba60345
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-07-03 08:01:57 -0300

    Feat: Dark mode added and animation card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements AfterViewInit {
+export class App implements AfterViewInit, OnInit {
+
   protected title = 'front';
+  isDarkMode = false;
   isDrawerOpen = false;
   productos = [
     {
@@ -45,6 +47,14 @@ export class App implements AfterViewInit {
       stock: false
     }
   ];
+
+
+  ngOnInit(): void {
+    this.configDarkMode();
+    this.updateDarkModeClass();
+  }
+
+
   ngAfterViewInit() {
     // Le damos un pequeño delay para evitar conflictos
     setTimeout(() => {
@@ -54,5 +64,48 @@ export class App implements AfterViewInit {
         easing: 'ease-in-out',
       });
     }, 0);
+  }
+
+
+  /**
+   * Configura el modo oscuro según la preferencia del usuario o del sistema
+   * y actualiza la clase del HTML.
+   */
+  configDarkMode() {
+    // Detecta si el sistema está en modo oscuro
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // Si el usuario ya eligió un modo antes, respetalo
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+      this.isDarkMode = savedTheme === 'dark';
+    } else {
+      this.isDarkMode = prefersDark;
+    }
+  }
+
+  /**
+   * Toggle dark mode
+   */
+  updateDarkModeClass(): void {
+    const html = document.documentElement;
+    if (this.isDarkMode) {
+      html.classList.add('dark');
+    } else {
+      html.classList.remove('dark');
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    const htmlElement = document.documentElement;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+
+    if (this.isDarkMode) {
+      htmlElement.classList.add('dark');
+    } else {
+      htmlElement.classList.remove('dark');
+    }
   }
 }
